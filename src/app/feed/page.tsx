7eb79f83a6238b7090d084fa8320489997977c87
Guney Unay <guneymarketing17@hotmail.com>
2024-11-03 18:15:16 +0300

    :lipstick: ui changes

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function FeedPage() {
-  return <div>FeedPage</div>
+import { db } from '@/db'
+import { promptsTable } from '@/db/schema'
+
+export default async function FeedPage() {
+  const data = await db.select().from(promptsTable)
+  return (
+    <div>
+      {data.map((row) => (
+        <div key={row.id}>
+          <Image width={500} height={500} alt={row.promt} src={row.image_url} />
+        </div>
+      ))}
+    </div>
+  )
 }
