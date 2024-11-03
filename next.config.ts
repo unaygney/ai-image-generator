5c9d627cfbaab9c70238714e6f54e359ada7d57d
Guney Unay <guneymarketing17@hotmail.com>
2024-11-03 18:14:41 +0300

    :wrench: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    loader: 'custom',
+    loaderFile: './src/components/loader.tsx',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`,
+        port: '',
+        pathname: '/storage/v1/render/image/public/**',
+      },
+    ],
+  },
   experimental: {
     typedRoutes: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
